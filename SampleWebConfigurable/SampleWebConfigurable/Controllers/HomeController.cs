4002c27d2f3d30e0d4c64e7087ee336b1b20864c
Dennis Moon <dmoon396@hotmail.com>
2019-02-05 10:42:24 -0600

    initial checkin

---
+++ SampleWebConfigurable/SampleWebConfigurable/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using SampleWebConfigurable.Models;
+
+namespace SampleWebConfigurable.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IConfiguration _config;
+        private readonly IHostingEnvironment _hostingEnv;
+
+        public HomeController(
+            IConfiguration config,
+            IHostingEnvironment hostingEnv)
+        {
+            _config = config;
+            _hostingEnv = hostingEnv;
+        }
+
+        public IActionResult Index()
+        {
+            var viewModel = new HomeViewModel();
+
+            viewModel.EnvironmentName = _hostingEnv.EnvironmentName;
+            viewModel.HostName = System.Net.Dns.GetHostName();
+            viewModel.MySetting = _config["MySetting"];
+            viewModel.MySecret = "<NOT FOUND>";
+
+            var mySecret = "";
+
+            if (System.IO.File.Exists(viewModel.MySecretFile))
+            {
+                mySecret = System.IO.File.ReadAllText(viewModel.MySecretFile);
+            }
+            else
+            {
+                if (_hostingEnv.EnvironmentName.Equals("Development"))
+                {
+                    mySecret = _config["MySecret"];
+                }
+            }
+
+            viewModel.MySecret = mySecret;
+
+            return View(viewModel);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ SampleWebConfigurable/SampleWebConfigurable/Models/HomeViewModel.cs
@@ -0,0 +1,14 @@
+﻿
+namespace SampleWebConfigurable.Models
+{
+    public class HomeViewModel
+    {
+        public string TestSetting { get; set; }
+
+        public string EnvironmentName { get; set; }
+        public string HostName { get; set; }
+        public string MySetting { get; set; }
+        public string MySecret { get; set; }
+        public string MySecretFile = "/kvmnt/MySecret";
+    }
+}
